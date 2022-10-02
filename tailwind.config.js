c1a38abe620693c0b981492880f2d54b5f246e46
Oluwaphemmy <ojokofemi@gmail.com>
2022-10-02 19:08:19 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        title: ["Sansita", "sans-serif"],
+        body: ["Sansita", "sans-serif"],
+      },
+      colors: {
+        pri: "#081F4D",
+        sec: "#0083FF",
+        gen: "#3B4D71",
+        back: "#D9E6FF",
+      },
+    },
+  },
+  plugins: [],
+};
